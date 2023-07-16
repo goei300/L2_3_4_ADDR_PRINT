142e25af3daf27ce1161f5146176b7cb5f12c828
signal_y <tlsgh08@naver.com>
2023-07-16 23:32:08 +0900

    ip_addr_patch

--- pcap-test.c
+++ pcap-test.c
@@ -2,6 +2,8 @@
 #include <stdbool.h>
 #include <stdio.h>
 #define ETHER_ADDR_LEN 6
+#define LIBNET_LIL_ENDIAN 1
+
 
 struct libnet_ethernet_hdr
 {
@@ -10,8 +12,101 @@ struct libnet_ethernet_hdr
     u_int16_t ether_type;                 /* protocol */
 };
 
-void print_addr(u_int8_t *pac){
-	printf("%x",pac);
+struct libnet_ipv4_hdr
+{
+#if (LIBNET_LIL_ENDIAN)
+    u_int8_t ip_hl:4,      /* header length */
+           ip_v:4;         /* version */
+#endif
+#if (LIBNET_BIG_ENDIAN)
+    u_int8_t ip_v:4,       /* version */
+           ip_hl:4;        /* header length */
+#endif
+    u_int8_t ip_tos;       /* type of service */
+#ifndef IPTOS_LOWDELAY
+#define IPTOS_LOWDELAY      0x10
+#endif
+#ifndef IPTOS_THROUGHPUT
+#define IPTOS_THROUGHPUT    0x08
+#endif
+#ifndef IPTOS_RELIABILITY
+#define IPTOS_RELIABILITY   0x04
+#endif
+#ifndef IPTOS_LOWCOST
+#define IPTOS_LOWCOST       0x02
+#endif
+    u_int16_t ip_len;         /* total length */
+    u_int16_t ip_id;          /* identification */
+    u_int16_t ip_off;
+#ifndef IP_RF
+#define IP_RF 0x8000        /* reserved fragment flag */
+#endif
+#ifndef IP_DF
+#define IP_DF 0x4000        /* dont fragment flag */
+#endif
+#ifndef IP_MF
+#define IP_MF 0x2000        /* more fragments flag */
+#endif 
+#ifndef IP_OFFMASK
+#define IP_OFFMASK 0x1fff   /* mask for fragmenting bits */
+#endif
+    u_int8_t ip_ttl;          /* time to live */
+    u_int8_t ip_p;            /* protocol */
+    u_int16_t ip_sum;         /* checksum */
+    struct in_addr ip_src, ip_dst; /* source and dest address */
+};
+
+struct libnet_tcp_hdr
+{
+    u_int16_t th_sport;       /* source port */
+    u_int16_t th_dport;       /* destination port */
+    u_int32_t th_seq;          /* sequence number */
+    u_int32_t th_ack;          /* acknowledgement number */
+#if (LIBNET_LIL_ENDIAN)
+    u_int8_t th_x2:4,         /* (unused) */
+           th_off:4;        /* data offset */
+#endif
+#if (LIBNET_BIG_ENDIAN)
+    u_int8_t th_off:4,        /* data offset */
+           th_x2:4;         /* (unused) */
+#endif
+    u_int8_t  th_flags;       /* control flags */
+#ifndef TH_FIN
+#define TH_FIN    0x01      /* finished send data */
+#endif
+#ifndef TH_SYN
+#define TH_SYN    0x02      /* synchronize sequence numbers */
+#endif
+#ifndef TH_RST
+#define TH_RST    0x04      /* reset the connection */
+#endif
+#ifndef TH_PUSH
+#define TH_PUSH   0x08      /* push data to the app layer */
+#endif
+#ifndef TH_ACK
+#define TH_ACK    0x10      /* acknowledge */
+#endif
+#ifndef TH_URG
+#define TH_URG    0x20      /* urgent! */
+#endif
+#ifndef TH_ECE
+#define TH_ECE    0x40
+#endif
+#ifndef TH_CWR   
+#define TH_CWR    0x80
+#endif
+    u_int16_t th_win;         /* window */
+    u_int16_t th_sum;         /* checksum */
+    u_int16_t th_urp;         /* urgent pointer */
+};
+
+void print_MACaddr(u_int8_t *pac){
+	printf("%02x:%02x:%02x:%02x:%02x:%02x\n",pac[0],pac[1],pac[2],pac[3],pac[4],pac[5]);
+}
+
+void print_IP_addr(struct in_addr pac){
+	char* src_ip_order= inet_ntoa(pac);
+	printf("%s\n",src_ip_order);
 }
 void usage() {
 	printf("syntax: pcap-test <interface>\n");
@@ -55,14 +150,28 @@ int main(int argc, char* argv[]) {
 			printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(pcap));
 			break;
 		}
-		struct libnet_ethernet_hdr *ether_hdr = (struct libnet_ehternet_hdr *)packet;
+		// Declare each layer's header
+		struct libnet_ethernet_hdr *ether_hdr = (struct libnet_ethernet_hdr *)packet;
+		struct libnet_ipv4_hdr *ip_hdr = (struct libnet_ipv4_hdr *)(packet + sizeof(struct libnet_ethernet_hdr));
+
 		printf("%u bytes captured\n", header->caplen);
 		
-		//print_addr(ether_hdr->ether_dhost);
-		printf("%d",len(ether_hdr->ether_dhost));
-		printf("\n");
-		//print_addr(ether_hdr->ether_shost);
-		printf("\n");
+		// Ethernet header
+		printf("Ethernet addr : \n");
+		printf("destination addr : ");
+		print_MACaddr(ether_hdr->ether_dhost);
+		printf("source addr : ");
+		print_MACaddr(ether_hdr->ether_shost);
+
+
+		// Ip header
+		printf("IP addr : \n");
+		printf("destination addr : ");
+		print_IP_addr(ip_hdr->ip_src);
+		//printf("ip hdr[0] : %d\n",ip_hdr->ip_src);
+		printf("destination addr : ");
+		print_IP_addr(ip_hdr->ip_dst);
+		
 	}
 
 	pcap_close(pcap);
